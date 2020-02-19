c82f28f08ff838cdbb0397cefa9d8560a2602ef0
KrisHugo <krishugo@users.noreply.github.com>
2020-02-19 15:16:22 +0800

    Binding Bootstrap with vue using bootstrap-vue Fix dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+/*
+* 入口JS
+* */
+/*
+* 引入Bootstrap 和 Bootstrap-vue
+* */
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+import './custom.scss';
